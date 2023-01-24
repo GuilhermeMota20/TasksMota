e4194c505678e0ed18478bb60b73b7ddd75fb0f7
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2023-01-24 12:03:04 -0300

    [FRONTEND] FT - ajustes no fechamento do modal de nova tarefa

--- src/components/Modals/index.tsx
+++ src/components/Modals/index.tsx
@@ -5,15 +5,22 @@ interface ModalProps {
     children: React.ReactNode;
     title: string;
     onClose: () => void;
+    isOpen?: boolean;
 }
 
-export default function Modal({ children, title, onClose }: ModalProps) {
+export default function Modal({ children, title, onClose, isOpen }: ModalProps) {
+    const closeModalHandler = (event: React.MouseEvent) => {
+        if (event.target === event.currentTarget) {
+            onClose();   
+        };
+    };
+
     return (
         <div
-            className="xl:text-base sm:text-sm text-xs fixed top-0 left-0 bg-slate-600/[.2] w-full h-full z-40 grid place-items-center px-2 text-slate-600"
-            onClick={onClose}
+            className="xl:text-base sm:text-sm text-xs fixed bg-slate-600/[.2] top-0 left-0 w-full h-full z-40 grid place-items-center px-2 text-slate-600"
+            onClick={closeModalHandler}
         >
-            <section className="relative bg-slate-200 max-w-lg w-full rounded-lg p-3 sm:p-5 flex flex-col justify-star">
+            <section className="relative bg-slate-200 max-w-lg w-full rounded-md p-3 sm:p-5 flex flex-col justify-star">
                 <button
                     aria-label="close alert"
                     className="absolute right-3 sm:right-4 rounded-md transition p-2 hover:bg-slate-100  hover:shadow-sm"
