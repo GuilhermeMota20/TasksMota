f98863a9d7eebf3c6280c2f02eff6b6877c64797
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2023-09-15 20:23:52 -0300

    Refactor: remove RootHead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { usePathname } from 'next/navigation';
 import { QueryClientProvider } from 'react-query';
 import ProtectedRoute from './components/Utilities/ProtectedRoute';
 import { AuthContextProvider } from './context/AuthContext';
-import RootHead from './head';
 import { queryClient } from './services/queryClient';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -21,7 +20,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <html lang="pt-br">
-        <RootHead />
+        <head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+          <link rel="icon" type="image/x-icon" href="./favicon.svg" />
+          <title>ToDoTasks.</title>
+        </head>
+
         <AuthContextProvider>
           <QueryClientProvider client={queryClient}>
             <body className="bg-slate-200 dark:bg-darkBlue-900 select-none">
